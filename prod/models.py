671ea4c2016c1890b4da2778fd89ebc3f264a5db
opportuna___ <StudentBI@SYN13ASS-NB-507.alabuga.local>
2024-12-27 14:24:18 +0300

    create UserModel

--- prod/models.py
+++ prod/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+adm_choices = (('yes','yes'),('no','no'))
+
+
+class UserModel(models.Model):
+    name = models.CharField(max_length=48)
+    surname = models.CharField(max_length=48)
+    phone = PhoneNumberField(region='RU', blank=True)
+    mail = models.EmailField()
+    age = models.PositiveIntegerField()
+    password = models.CharField(max_length=255)
+    is_admin = models.CharField(max_length=3,choices=adm_choices)
