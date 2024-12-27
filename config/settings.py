78b0dce2e307ff248864599e42d7b12ef4cd8aa3
opportuna___ <StudentBI@SYN13ASS-NB-507.alabuga.local>
2024-12-27 14:19:08 +0300

    add app in setting

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
     'dev.apps.DevConfig',
     'prod.apps.ProdConfig',
 ]
