037ca18f706adbabd733e5eee496d327f9b0a56e
StudentBI1 <StudentBI1@SYN13-NB-1501.alabuga.local>
2024-12-27 15:08:04 +0300

    create_view

--- prod/views.py
+++ prod/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Product
-from .forms import ProductForm
+from ..dev.forms import ProductForm
 from .views import generic
 
